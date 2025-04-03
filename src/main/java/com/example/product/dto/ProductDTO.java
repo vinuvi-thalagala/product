e0f70119f5620c4385645b4dd202781ca6a51527
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2025-04-03 15:55:12 +0530

    create Product dto

---
+++ src/main/java/com/example/product/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.example.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProductDTO {
+   
+    private int id;
+    private int productId;
+    private String productName;
+    private String description;
+    private int forSale;
+}
